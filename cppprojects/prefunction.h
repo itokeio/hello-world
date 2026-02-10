fcad025d55966ce9b8254703e12ac0ea01cefab0
Itokeio <itokeio41506066@gmail.com>
2026-02-10 23:07:50 +0800

    first implementation

--- cppprojects/prefunction.h
+++ cppprojects/prefunction.h
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<stdbool.h>
+#include<string.h>
 
-#define OK 0
+#define OK 1
 #define OVER 2
-#define FATAL 1
+#define INVALID -1
+#define FATAL -2
 
 //读取字符串的函数
 int read_line(char place[] , int n)
@@ -18,7 +21,7 @@ int read_line(char place[] , int n)
 }
 
 //输出字符串的函数
-int print_line(char * place,int n)
+int print_line(char place[] , int n)
 {
     for(int i=0 ; place[i]!='\0' ; i++)
     {
@@ -27,5 +30,82 @@ int print_line(char * place,int n)
     return OK;
 }
 
+//判断用户输入的符号是Yes还是No
+int yes_or_no()
+{
+    char exit[5];
+    memset(exit,'\0',sizeof(exit));
+    read_line(exit,sizeof(exit));
+
+    if(exit[3]!='\0') 
+    {
+        memset(exit,'\0',sizeof(exit));
+        return INVALID;
+    }else if(exit[0]=='Y'&&exit[1]=='e'&&exit[2]=='s') 
+    {
+        memset(exit,'\0',sizeof(exit));
+        return true;
+    }else if(exit[0]=='N'&&exit[1]=='o'&&exit[2]=='\0') 
+    {
+        memset(exit,'\0',sizeof(exit));
+        return false;
+    }else 
+    {
+        memset(exit,'\0',sizeof(exit));
+        return INVALID;
+    }
+}
 
 
+/*------------------线性表线性表示专用函数-----------------*/
+
+//宏定义数字
+#define MAX_LETTER 50
+
+//定义单独学生信息的结构体
+typedef struct
+{
+    int id;
+    char name[MAX_LETTER];
+    int maths;
+    int chinese;
+    int english;
+} student;
+
+//建立动态数组结构体
+typedef struct
+{
+    student *elem;
+    int length;
+    int listsize;
+} Sqlist;
+
+//创建一个指定长度的线性表
+int sqlist_init(int n, Sqlist *list)
+{
+    if (list == NULL || n <= 0 || n > 1000)
+        return OVER;
+
+    list->elem = malloc(sizeof(student) * n);
+    if (list->elem == NULL)
+        return OVER;
+
+    memset(list->elem, 0, sizeof(student)*n);
+    list->length = 0;
+    list->listsize = n;
+    return OK;
+}
+
+//销毁线性表
+int sqlist_destory(Sqlist *list)
+{
+    if (list == NULL)
+        return OVER;
+    free(list->elem);
+    list->elem = NULL;
+
+    list->length = 0;
+    list->listsize = 0;
+
+    return OK;
+}
--- cppprojects/sort.c
+++ cppprojects/sort.c
@@ -1,32 +1,80 @@
-//这是一个线性表，其中涉及了许多基本运算
+// Exercise on linear representation and implementation of linear lists
 
-#include<stdio.h>
-#include<stdlib.h>
-#include"Prefunction.h" 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+//引用我另行制作的文件中的函数
+#include "Prefunction.h"
 
-//宏定义数字
-#define MAX_NUMBER 10
-#define MAX_LETTER 50
-
-//定义单独学生信息的结构体
-typedef struct{
-    int id ;
-    char name[MAX_LETTER];
-    int maths;
-    int chinese;
-    int english;
-} student;
-
-//创建一个指定长度的线性表
-int splist_init(int n)
+//提供帮助菜单
+int sqlist_help()
 {
-    student list[n];
+    printf("===================== HELP MENU ======================\n");
+    printf("| Number |         Operation Description             |\n");
+    printf("|--------|-------------------------------------------|\n");
+    printf("|   0    | expand the help menu                      |\n");
+    printf("|   1    | Add a new student's information           |\n");
+    printf("|        | (Include ID, name, maths/chinese/english) |\n");
+    printf("|   2    | Query student information (by ID)         |\n");
+    printf("|   3    | Modify student's scores                   |\n");
+    printf("|   4    | Delete a student's information (by ID)    |\n");
+    printf("|   5    | Display all students' information         |\n");
+    printf("|   6    | Clear the list (keep program running)     |\n");
+    printf("|   9    | Exit the program (also destroy the list)  |\n");
+    printf("======================================================\n");
+    printf("You can enter 0~9 to operate the list\n");
     return OK;
+}
+
+//初始化列表
+int initlist(Sqlist *list)
+{
+    while (true)
+    {
+        int start = 0;
+        scanf("%d", &start);
+        int reinit = sqlist_init(start,list);
+
+        if (reinit == OK)
+        {
+            printf("A list with a length of %d has been successfully created.\n", start);
+            break;
+        }
+
+        else
+        {
+            printf("Creation failed. The length must be 1~1000. Please re-enter.\n");
+        }
+    }
+    return OK;
+}
+
+//9：退出并销毁列表
+int exit_9(Sqlist *list)
+{
+    printf("Are you sure you want to exit the program? Doing so will also destroy the list you have created,Enter Yes or No: ");
+    while (true)
+    {
+        int exit_result = yes_or_no();
+        if (exit_result == true)
+        {
+            sqlist_destory(list);
+            printf("Program terminated.Thank you for your use.\n");
+            return true;
+        }
+        if (exit_result == false)  
+        {
+            printf("Exit cancelled, return to main operation.\n");
+            return false;
+        }
+        if (exit_result == INVALID) printf("Invalid input! Please enter Yes/No only.\n");
+    }
 }
 
 
 
-//查找线性表中的元素
+/*查找线性表中的元素
 int sqlist_find(char a)
 {
     for(int i=0 ; i<)
@@ -38,24 +86,48 @@ int sqlist_find(char a)
 //修改线性表中的元素
 int sqlist_change(char a,)
 {
-    
 
-}
+
+} */
 
 int main()
 {
+
+    Sqlist L;
+
     printf("Welcome to the system which I develop to manage the students' informations.\n");
     printf("The first thing that we must do is to create a empty list to save informations.\n");
-    printf("Please enter the length which of the list you want to start creating.");
+    printf("Please enter the length which of the list you want to start creating.\n");
 
-    int start=0;
-    student sqlist[0];
-    scanf("%d",&start);
-    sqlist_init(start);
+    initlist(&L);
 
+    printf("Now you can perform various operations by entering numbers.\n");
 
-    return 0;
+    for (int i=0 ; ; i++)
+    {
+        if(i!=0) printf("Enter a number to operate: ");
+        if(i==0) printf("Tip: Enter 0 to view help if you don't know how to operate.\n");
+        int judge=0;
+        scanf("%d",&judge);
+        getchar();
+
+        switch (judge)
+        {
+        case 0:
+            sqlist_help();
+            break;
+
+        case 9:
+        {
+            int ex9 = exit_9(&L);
+            if(ex9==true) return 0;
+            else break;
+        }
+        default: 
+            printf("Invalid input! Enter 0 to view help menu.\n");
+            break;
+        }
+    }
 }
 
-
-//printf("Please enter the ID of student and what infomation you want to change");
+// printf("Please enter the ID of student and what infomation you want to change");
